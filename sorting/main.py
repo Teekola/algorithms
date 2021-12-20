b2314d60016c5b83c6c5009691075334be85c85b
Teemu Kerkola <tokerk@utu.fi>
2021-12-20 23:09:03 +0200

    Added Quick Sort

--- sorting/main.py
+++ sorting/main.py
@@ -1,14 +1,26 @@
 from time import perf_counter as pfc
-from insertionSort import insertionSort;
+from insertionSort import *
+from quickSort import *
 
 # Run tests
 if __name__ == '__main__':
 
-    # Insertion sort          
-    print("INSERTION SORT")     
-    A = [9, 7, 1, 5, 2, 6, 4]   # Test array
+    # Insertion Sort          
+    print("INSERTION SORT")
+    # Test array 
+    A = [460, 4, 207, 97, 345, 221, 109, 59, 468, 247, 192, 193, 304, 217, 491, 208, 291, 403, 65, 80, 369, 256, 14, 187, 326, 449]   
     print("BEFORE:", A)         # Print the list before sorting
     start_time = pfc()          # Get the start time
     A = insertionSort(A)        # Run the algorithm
     print(pfc() - start_time)   # Print the time
     print("AFTER:", A)          # Print the list after sorting
+
+    # Quick Sort
+    print("QUICK SORT")
+    # Test array
+    A = [460, 4, 207, 97, 345, 221, 109, 59, 468, 247, 192, 193, 304, 217, 491, 208, 291, 403, 65, 80, 369, 256, 14, 187, 326, 449]       
+    print("BEFORE:", A)             # Print the list before sorting
+    start_time = pfc()              # Get the start time
+    A = quickSort(A, 0, len(A)-1)   # Run the algorithm
+    print(pfc() - start_time)       # Print the time
+    print("AFTER:", A)              # Print the list after sorting
---
+++ sorting/quickSort.py
@@ -0,0 +1,40 @@
+def swap(array, i, j):
+    A = array.copy()    # Copy the array to prevent unwanted side-effects
+    temp = A[i]         # Store item at i to temp
+    A[i] = A[j]         # Put item at j to index i
+    A[j] = temp         # Put the value stored in temp to index j
+    return A            # Return resulting array
+
+
+def partition(array, p, r):
+    A = array.copy()    # Copy the array to prevent unwanted side-effects
+
+    x = A[r]            # Make the last item the pivot
+    i = p-1             # Counter i increases when we find an item smaller than the pivot
+
+    # Counter j goes from left index to the right index
+    for j in range(p, r):
+
+        # If the item at j is smaller or equal to the pivot
+        if A[j] <= x:
+            i += 1              # incerase counter i
+            A = swap(A, i, j)   # Swap items at i and j
+    
+    # Swap the pivot to its correct position
+    A = swap(A, r, i + 1)
+
+    # Return the partitioned array and the pivot's index
+    return A, i + 1
+
+
+def quickSort(array, p, r):
+    A = array.copy()     # Copy the array to prevent unwanted side-effects
+
+    # While the left and right index bound a list of length 2 or higher
+    if p < r:
+        A, q = partition(A, p, r)   # Partition the array and get the pivot index
+        A = quickSort(A, p, q - 1)  # Sort the left subarray recursively
+        A = quickSort(A, q + 1, r)  # Sort the right subarray recursively
+
+    # Return the sorted array
+    return A
